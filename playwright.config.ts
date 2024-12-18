6062c2b5150dadf4de3e12b0155eeccc2d8e6dbf
Mike Hartington <mikehartington@gmail.com>
2024-12-18 14:22:27 -0500

    cache tests and depend on build

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'npm run start',
+		command: 'npx nx start',
 		url: 'http://localhost:4200',
 		reuseExistingServer: !process.env['CI'],
 	},
